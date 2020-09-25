029b08e6fcfcb14532c722668a1fc0b4dcee7bd4
Ilnar Khafizov <ilnar_khafizov@epam.com>
2020-09-25 14:19:12 +0400

    added ListenService and ParseService

---
+++ src/main/java/com/example/service/ListenService.java
@@ -0,0 +1,34 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Scanner;
+
+@Service
+public class ListenService {
+
+    @Autowired
+    private DisplayService displayService;
+    @Autowired
+    private ParseService parseService;
+
+
+    public void run() {
+        String[] exception;
+        Scanner scanner;
+        Map<String, String> data;
+
+        while (true) {
+            scanner = new Scanner(System.in);
+            exception = scanner.nextLine().split(" ");
+            if (exception[0].equals("exit")) {
+                System.exit(0);
+            }
+            data = this.parseService.parseExpression(exception);
+            this.displayService.print(data);
+            break;
+        }
+    }
+}
---
+++ src/main/java/com/example/service/ParseService.java
@@ -0,0 +1,98 @@
+package com.example.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class ParseService {
+
+    private final Map<String, String> data = new HashMap<>();
+    private String[] expression;
+
+    private static final String ERROR_KEY = "error";
+    private static final String DEFAULT_ORDER = "time";
+    private static final String DEFAULT_FILTER = "off";
+
+    public Map<String, String> parseExpression(String[] expression) {
+        this.expression = expression;
+        switch (this.expression[0]) {
+            case "show":
+                this.data.put("action", "show");
+                this.show();
+                break;
+            case "":
+                this.data.put(ERROR_KEY, "");
+                break;
+            default:
+                this.data.put(ERROR_KEY, "Некорректное выражение! Для выхода введите команду \"exit\"!");
+        }
+        return this.data;
+    }
+
+    private void show() {
+        if (this.expression.length < 2) {
+            this.data.put(ERROR_KEY, "Не указан объект для отображения! Пример: \"student lastname_firstname\" или \"students\"!");
+            return;
+        }
+        switch (this.expression[1]) {
+            case "student":
+                this.data.put("object", "student");
+                if (this.expression.length < 3) {
+                    this.data.put(ERROR_KEY, "Не указано имя студента! Пример: \"Ivanov_Ivan\"");
+                    return;
+                }
+                this.data.put("fullName", this.parseName(this.expression[2]));
+                break;
+            case "students":
+                this.data.put("object", "students");
+                this.addOrder();
+                this.addFilter();
+                break;
+            default:
+                this.data.put(ERROR_KEY, "Некорректно указан объект для отображения! Пример: \"student lastname_firstname\" или \"students\"!");
+        }
+    }
+
+    private void addOrder() {
+        String order = "order";
+        for (String word : this.expression) {
+            if (word.startsWith("order=")) {
+                switch (word.replace("order=", "")) {
+                    case "time":
+                        this.data.put(order, "time");
+                        return;
+                    case "average":
+                        this.data.put(order, "average");
+                        return;
+                    default:
+                        this.data.put(ERROR_KEY, "Некорректно указан способ сортировки! Пример: \"order=time\" или \"order=average\"");
+                        return;
+                }
+            }
+        }
+        this.data.put(order, DEFAULT_ORDER);
+    }
+
+    private void addFilter() {
+        String filter = "filter";
+        for (String word : this.expression) {
+            if (word.startsWith("filter=")) {
+                switch (word.replace("filter=", "")) {
+                    case "may_continue":
+                        this.data.put(filter, "may_continue");
+                        return;
+                    default:
+                        this.data.put(ERROR_KEY, "Некорректно указан фильтр! Пример: \"filter=may_continue\"");
+                        return;
+                }
+            }
+        }
+        this.data.put(filter, DEFAULT_FILTER);
+    }
+
+    private String parseName(String name) {
+        return name.replace("_", " ");
+    }
+}
