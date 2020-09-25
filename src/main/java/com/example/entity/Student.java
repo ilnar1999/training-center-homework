d1c97c129f70d6364dd8cc9fdd6086546a129873
Ilnar Khafizov <ilnar_khafizov@epam.com>
2020-09-25 16:36:50 +0400

    updated Student class

--- src/main/java/com/example/entity/Student.java
+++ src/main/java/com/example/entity/Student.java
@@ -1,6 +1,5 @@
 package com.example.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
 
@@ -9,7 +8,6 @@ import java.util.List;
 import java.util.Objects;
 
 @Data
-@AllArgsConstructor
 public class Student {
 
     @NonNull
@@ -20,6 +18,48 @@ public class Student {
     private LocalDate startDate;
     @NonNull
     private List<Integer> marks;
+    @NonNull
+    private int countOfDaysUntilFinishCurriculum;
+    @NonNull
+    private double averageMark;
+    @NonNull
+    private boolean mayBeContinueTraining;
+
+
+    public Student(@NonNull String fullName, @NonNull Curriculum curriculum, @NonNull LocalDate startDate, @NonNull List<Integer> marks) {
+        this.fullName = fullName;
+        this.curriculum = curriculum;
+        this.startDate = startDate;
+        this.setMarks(marks);
+    }
+
+
+    public void setMarks(List<Integer> marks) {
+        this.marks = marks;
+        this.setCountOfDaysUntilFinishCurriculum();
+        this.setAverageMark();
+        this.setMayBeContinueTraining();
+    }
+
+    public void setCountOfDaysUntilFinishCurriculum() {
+        int totalCountOfHours = curriculum.getCourses().stream().mapToInt(Course::getDuration).sum();
+        int totalCountOfDays = totalCountOfHours % 8 == 0 ? totalCountOfHours / 8 : totalCountOfHours / 8 + 1;
+        int countOfFinishedDays = this.marks.size();
+        this.countOfDaysUntilFinishCurriculum =  totalCountOfDays - countOfFinishedDays;
+    }
+
+    public void setAverageMark() {
+        this.averageMark = marks.stream().mapToInt(mark -> mark).average().orElse(0);
+        this.averageMark = (int) (averageMark * 10) / 10.0;
+    }
+
+    public void setMayBeContinueTraining() {
+        int countCurrentMarks = this.marks.size();
+        int countRemainingMarks = this.countOfDaysUntilFinishCurriculum;
+        int totalCountOfDays = countCurrentMarks + countCurrentMarks;
+        double maxAverageMark = (countCurrentMarks * this.averageMark + countRemainingMarks * 5) / totalCountOfDays;
+        this.mayBeContinueTraining = maxAverageMark >= 4.5;
+    }
 
     @Override
     public boolean equals(Object o) {
